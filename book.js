0556dfc72bf04e0c93264e0291a7d7b274a38048
groovygnome <johnrperez810@gmail.com>
2025-03-20 17:42:06 -0400

    add functionality to new book button, allow user-specified books to be displayed on page. add delete button to each book entry to delete book card, and add read button to each book entry to toggle read status

--- book.js
+++ book.js
@@ -2,6 +2,8 @@ let lib = [];
 const container = document.querySelector('.container');
 const bookBtn = document.querySelector('.new-book');
 const modal = document.querySelector('.book-form');
+const modalClose = document.querySelector('.close');
+const submitBtn = document.querySelector('.submit');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -20,25 +22,69 @@ function Book(title, author, pages, read) {
     }
 }
 
+Book.prototype.isRead = function() {
+    if (this.read) {
+        this.read = false;
+    } else {
+        this.read = true;
+    }
+}
+
 function addBookToLibrary(library, title, author, pages, read) {
     library.push(new Book(title, author, pages, read));
 
 }
 
+
 function displayBooks(library) {
 
     for (let book of library) {
-
-        const card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        card.setAttribute('id', book.title);
-        card.textContent = book.info();
-        container.appendChild(card);
+        displayBook(book);
     }
-    document.body.appendChild(container);
+}
+
+function displayBook(book) {
+    const card = document.createElement('div');
+    const bookInfo = document.createElement('span');
+    card.setAttribute('class', 'card');
+    const id = book.title.replace(/\s+/g, '');
+    card.setAttribute('id', id);
+    bookInfo.textContent = book.info();
+    card.appendChild(bookInfo);
+
+    const btns = document.createElement('div');
+    const delBtn = document.createElement('button');
+    const readBtn = document.createElement('button');
+    btns.appendChild(delBtn);
+    btns.appendChild(readBtn);
+
+    delBtn.textContent = 'Delete';
+    delBtn.addEventListener('click', () => { container.removeChild(document.querySelector('#' + id)); });
+
+    readBtn.textContent = 'Read';
+    readBtn.addEventListener('click', () => { book.isRead(); bookInfo.textContent = book.info(); });
+
+    card.appendChild(btns);
+
+    container.appendChild(card);
 }
 
 bookBtn.addEventListener('click', () => { modal.show(); });
+modalClose.addEventListener('click', () => { modal.close(); event.preventDefault(); });
+submitBtn.addEventListener('click', () => {
+    modal.close();
+    event.preventDefault();
+    let book = new Book(document.querySelector('#title').value, document.querySelector('#author').value, document.querySelector('#pages').value, document.querySelector('#read').checked);
+    displayBook(book);
+    lib.push(book);
+
+
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+    document.querySelector('#read').checked = false;
+});
+
 
 addBookToLibrary(lib, 'The Hobbit', 'J.R.R. Tolkien', 295, false);
 
